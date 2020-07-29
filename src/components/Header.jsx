ab5b666dd262ddf9510ecf179cbd11ebd2bab91c
ullsee <61946112+ullsee@users.noreply.github.com>
2020-07-30 01:33:56 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import SubjectIcon from "@material-ui/icons/Subject";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <SubjectIcon />
+        NoteBook
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
